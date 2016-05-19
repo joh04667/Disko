8931e89519242b1f32fd871ffc2ce6a144fdfab9
Peter Lilja-Sheeley <pliljasheeley@gmail.com>
2016-05-19 16:46:23 -0500

    work in progress of download dialogue using angular material

--- client/client.js
+++ client/client.js
@@ -1,1 +1,13 @@
-undefined
+var app = angular.module("app", ["ngMaterial"]);
+
+app.controller("MainController", ["$scope", "$http", function($scope, $http) {
+
+	var main = this;
+
+	main.downloadAlert = function(ev){
+
+    $mdDialog.show({
+      title: "Before downloading please fill out these questions"
+      templateUrl: 'downloadtemplate.html',
+      clickOutsideToClose:true
+    })
